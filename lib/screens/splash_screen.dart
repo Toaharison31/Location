f45bb10dfd74ec5af08cc49267bad25026a279a1
amboara14 <amboararatovo@gmail.com>
2026-07-30 11:06:31 +0300

    tsy iZy

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:animated_splah_screen'
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import native_splash_screen_macos
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NativeSplashScreenMacosPlugin.register(with: registry.registrar(forPlugin: "NativeSplashScreenMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
